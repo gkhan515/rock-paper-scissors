40ffc5a9146ac8e3b431f9366f974cb976612359
gkhan515 <khangibron@gmail.com>
2022-08-08 02:03:28 -0400

    create playRound function

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function getComputerChoice () {
   switch(Math.floor(Math.random() * 3)) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
   }
 }
 
-console.log(getComputerChoice());
+let playerSelection = "rOcK"
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if ((playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")) {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+  }
+
+  else if ((playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "rock")) {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+  }
+
+  else if (playerSelection === computerSelection){
+    console.log("It's a draw!")
+  }
+
+  else
+    console.log("you have entered nonsense")
+}
+
+playRound(playerSelection, getComputerChoice());
