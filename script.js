ec059a3ea2b7b698943da4681c7518d2fd28f281
gkhan515 <khangibron@gmail.com>
2022-09-19 01:38:34 -0400

    implement gui

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
+let wins = 0;
+let losses = 0;
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+const playerChoice = document.querySelector('.player-choice');
+const computerChoice = document.querySelector('.computer-choice');
+
+rock.addEventListener('click', () => {
+  playerChoice.innerHTML = "✊";
+  playRound("rock", getComputerChoice());
+});
+paper.addEventListener('click', () => {
+  playerChoice.innerHTML = "✋";
+  playRound("paper", getComputerChoice());
+});
+scissors.addEventListener('click', () => {
+  playerChoice.innerHTML = "✌️";
+  playRound("scissors", getComputerChoice());
+});
+
+const winCounter = document.querySelector('.win-counter');
+const lossCounter = document.querySelector('.loss-counter');
+const result = document.querySelector('.result');
+
 function getComputerChoice () {
   switch(Math.floor(Math.random() * 3)) {
     case 0:
+      computerChoice.innerHTML = "✊";
       return "rock";
     case 1:
+      computerChoice.innerHTML = "✋";
       return "paper";
     case 2:
+      computerChoice.innerHTML = "✌️";
       return "scissors";
   }
 }
@@ -15,39 +45,35 @@ function playRound(playerSelection, computerSelection) {
   if ((playerSelection === "rock" && computerSelection === "scissors") ||
       (playerSelection === "paper" && computerSelection === "rock") ||
       (playerSelection === "scissors" && computerSelection === "paper")) {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}`)
-    return 1;
-  }
+    result.innerHTML = `You win! ${playerSelection} beats ${computerSelection}`;
+    winCounter.innerHTML = `Player: ${++wins}`;
+  } else
 
   if ((playerSelection === "rock" && computerSelection === "paper") ||
       (playerSelection === "paper" && computerSelection === "scissors") ||
       (playerSelection === "scissors" && computerSelection === "rock")) {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
-    return 0;
-  }
+    result.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}`;
+    lossCounter.innerHTML = `CPU: ${++losses}`;
+  } else
 
   if (playerSelection === computerSelection){
-    console.log("It's a draw!")
-    return 0;
+    result.innerHTML = "It's a draw!";
   }
 
-  console.log("you have entered nonsense");
-  return 0;
+  checkGameOver();
 }
 
-function game () {
-  let wins = 0;
-  let losses = 0;
-  for(let i = 0;i < 5;i++) {
-    let playerSelection = prompt("Rock, paper, or scissors?")
-    if (playRound(playerSelection, getComputerChoice()))
-      wins++;
-    else
-      losses++;
+function checkGameOver () {
+  if ((wins == 5) || (losses == 5)) {
+    if (wins > losses) {
+      result.innerHTML = "You won!"
+    } else
+      result.innerHTML = "You Lost!"
+
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
   }
-  console.log(`Player: ${wins}`);
-  console.log(`Computer: ${losses}`);
-  console.log(wins > losses ? "You win!":"You lose!");
 }
 
-game()
+//game()
